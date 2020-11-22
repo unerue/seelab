0965ffc3a49fb0419e209d7ecb24fc2ee173a782
unerue <unerue@me.com>
2020-11-22 19:31:53 +0900

    added visualize coco

---
+++ seelab/visualize/visualize_coco.py
@@ -0,0 +1,356 @@
+import sys
+import json
+import os
+import random
+import argparse
+import math
+import tqdm
+from collections import defaultdict
+
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from matplotlib.lines import Line2D
+from matplotlib.patches import Polygon, Rectangle
+from matplotlib.collections import PatchCollection
+
+import pycocotools
+from pycocotools import mask
+# from skimage import measure
+
+# from config import rgb2rgba, CONSTRUCTION_CLASSES, CONSTRUCTION_COLORS_MAP, COLORS, COCO_CLASSES, COCO_LABEL_MAP
+
+# - arg_parse(인풋 = custom, coco, box, polygon, score_parameter, Num_images)
+# - bbox_custom_0.png, bbox_custom_0_15.png, poly_custom_0.png, poly_custom_0_15.png
+# - bbox_coco_0.png, bbox_coco_0_15.png, poly_coco_0.png, poly_coco_0_15.png 등등
+
+parser = argparse.ArgumentParser(description='Visualization')
+parser.add_argument(
+    '--image_dir', required=True)
+parser.add_argument(
+    '--info_path', required=True)
+parser.add_argument(
+    '--annot_type', required=False, default='both', help='bbox, mask, both', type = str)
+parser.add_argument(
+    '--num_images', required=False, default=6, help='number of images to be displayed', type=int)
+
+args = parser.parse_args()
+if args.num_images > 9:
+    raise ValueError
+
+with open(args.info_path, 'r') as json_file:
+    annots = json.load(json_file)
+    classes = {v['id']: v['name'] for v in annots['categories']}
+
+
+def color_cache(func):
+    cached_colors = defaultdict()
+    def wrapping_function(*args):
+        k, v = func(*args)
+        if k not in cached_colors:
+            cached_colors[k] = v
+        return k, cached_colors[k]
+    return wrapping_function
+
+
+@color_cache
+def get_label_color_map(category_id):
+    label = classes[category_id]
+    color = tuple(round(random.random(), 3) for _ in range(3))
+    mask = color + (.5,)
+    bbox = color + (1.,)
+    colors = (mask, bbox)
+    return label, colors
+
+
+    #     # color = CONSTRUCTION_COLORS_MAP.get(label)
+    # else:
+    #     coco_colors_map = {}
+    #     i = 0
+    #     for label in COCO_LABEL_MAP.keys():
+    #         coco_colors_map[label] = rgb2rgba(COLORS[i], 0.2)
+    #         i += 1
+    #         if i >= len(COLORS):
+    #             i = 0
+    #     label = COCO_CLASSES[COCO_LABEL_MAP.get(data['category_id'], 0)]
+    #     color = coco_colors_map.get(COCO_LABEL_MAP.get(data['category_id'], ((0.96, 0.26, 0.21, 0.2), (0.96, 0.26, 0.21, 1.0))))
+
+    # return label, color
+
+
+def draw_ground_truth(image_id=None, ax=None, annot_type='bbox'):
+    for data in annots['annotations']:
+        if data['image_id'] == image_id:
+            if annot_type == 'bbox':
+                draw_bbox(ax, data)
+
+def draw_bbox(ax, data):
+    label, color = get_label_color_map(data['category_id'])
+    box = data['bbox']
+    # if color is None:
+    # color = ((0.96, 0.26, 0.21, 0.2), (0.96, 0.26, 0.21, 1.0))
+    ractangle = Rectangle((box[0], box[1]), box[2], box[3], fill=None, ec=color[1])
+    ax.add_patch(ractangle)
+    ax.annotate(
+        label, (box[0], box[1]), color='w', weight='bold', 
+        fontsize=3, ha='left', va='bottom',
+        bbox=dict(facecolor=color[1], edgecolor=color[1], pad=0.0))
+
+
+def main():
+    # Get image ID, and file names from ground truth data
+    images = {annot['id']: annot['file_name'] for annot in annots['images']}
+    
+    shuffle = True
+    if shuffle:
+        shuffled_ids = list(images.keys())
+        random.shuffle(shuffled_ids)
+    
+    images = {key: images[key] for key in shuffled_ids}
+
+    # fig, axes = plt.subplots(2, 2, dpi=50)
+    # print(math.gcd(10, 4))
+    sizes = [i for i in range(1, args.num_images+1) if args.num_images % i == 0]
+    figsize = []
+    for i in range(len(sizes), 0, -1):
+        for j in range(i, len(sizes)):
+            if sizes[i] * sizes[j] == args.num_images:
+                figsize.append((sizes[i], sizes[j]))
+            if len(figsize) > 1:
+                break
+                
+    try:
+        row, col = figsize[0]
+    except IndexError:
+        row, col = 1, args.num_images
+    # if args.num_images < 4:
+    #     row, col = 1, args.num_images
+    # else:
+    #     row, col = figsize[0]
+    # row, col = size[1:3]
+    print(figsize, row, col)
+    # print(int(num), int(num))
+
+    # fig, axes = plt.subplots(2, 4, dpi=50)
+    fig = plt.figure(dpi=200)
+    dpi = fig.get_dpi()
+   
+    # scale = .2
+    # i = 1
+    
+    for i, (image_id, file_name) in enumerate(images.items()):
+        image = Image.open(f'{args.image_dir}/{os.path.basename(file_name)}').convert('RGB')
+        w, h = image.size
+        if i == 0:
+            fig.set_size_inches(
+                w*col / dpi, 
+                h*row / dpi)
+        
+        ax = fig.add_subplot(int(f'{row}{col}{i+1}'))
+        ax.imshow(image)
+        ax.set_title(f'{image_id}({os.path.basename(file_name)})', fontsize=int(w*col/dpi))
+        ax.axis('off')
+
+        draw_ground_truth(image_id=image_id, ax=ax, annot_type='bbox')
+        # axes.flatten()[i].imshow(image)
+        # axes.flatten()[i].imshow(image)
+        
+        # draw_prediction(image_id=image_id, ax=axes.flatten()[i-1], annot_type=annot_type)
+        # draw_ground_truth(image_id=image_id, ax=axes.flatten()[i], annot_type='bbox')
+        # axes.flatten()[i].axis('off')
+        # axes.flatten()[i].axis('off')
+        
+
+        if i+1 == args.num_images:
+            break
+
+        # if i == (num_imgs * 2 - 1): # 39
+        #     break
+
+        
+    
+    fig.tight_layout()
+    # plt.show()
+    fig.savefig('test.png')
+    # if args.pop_up_img:
+    #     plt.show()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+# with open(f'data/{path}/bbox_detections.json', 'r') as json_file:
+#     bbox_detections = json.load(json_file)
+
+# with open(f'data/{path}/mask_detections.json', 'r') as json_file:
+#     mask_detections = json.load(json_file)
+
+# coco_img_root = '/home/youngwoon/Documents/Tutorial/yolact/data/coco/images/'
+
+
+
+# def shuffle():
+#     pass
+
+
+# def generate_center_point(boxes):
+#     x1, y1, w, h = boxes[0], boxes[1], boxes[2], boxes[3]
+#     cx = x1 + (w/2)
+#     cy = y1 + (h/2)
+#     return cx, cy
+
+# from typing import Tuple, List, Dict, Any, Optional
+
+# def get_label_color_map(data: Dict) -> Tuple:
+#     """ Get label color map
+
+#     argument
+#         data : 
+
+#     return
+#         lable, color
+#     """
+#     if path == 'custom':
+#         label = CONSTRUCTION_CLASSES[data['category_id']-1]
+#         color = CONSTRUCTION_COLORS_MAP.get(label)
+#     else:
+#         coco_colors_map = {}
+#         i = 0
+#         for label in COCO_LABEL_MAP.keys():
+#             coco_colors_map[label] = rgb2rgba(COLORS[i], 0.2)
+#             i += 1
+#             if i >= len(COLORS):
+#                 i = 0
+#         label = COCO_CLASSES[COCO_LABEL_MAP.get(data['category_id'], 0)]
+#         color = coco_colors_map.get(COCO_LABEL_MAP.get(data['category_id'], ((0.96, 0.26, 0.21, 0.2), (0.96, 0.26, 0.21, 1.0))))
+
+#     return label, color
+
+
+# def draw_bbox(ax, data):
+#     label, color = get_label_color_map(data)
+#     box = data['bbox']
+#     if color is None:
+#         color = ((0.96, 0.26, 0.21, 0.2), (0.96, 0.26, 0.21, 1.0))
+#     ractangle = Rectangle((box[0], box[1]), box[2], box[3], fill=None, ec=color[1])
+#     ax.add_patch(ractangle)
+#     ax.annotate(
+#         label, (box[0], box[1]), color='w', weight='bold', 
+#         fontsize=3, ha='left', va='bottom',
+#         bbox=dict(facecolor=color[1], edgecolor=color[1], pad=0.0))
+
+
+# def draw_mask(ax, data, pred: bool = True):
+#     label, color = get_label_color_map(data)
+#     polygons = data['segmentation']
+
+#     if pred:
+#         boxes = mask.toBbox(polygons)
+#         cx, cy = generate_center_point(boxes)
+#         binary_mask = mask.decode(polygons)
+#         contours = measure.find_contours(binary_mask, 0.5)
+#         for contour in contours:
+#             contour = np.flip(contour, axis=1)
+#             polygon = contour.ravel().tolist()
+#             polygon = np.array(polygon).reshape(-1, 2)
+#             if color is None:
+#                 color = ((0.96, 0.26, 0.21, 0.2), (0.96, 0.26, 0.21, 1.0)) 
+#             patch = Polygon(polygon, True, fc=color[0], ec=color[1], lw=0.5)
+#             ax.add_patch(patch)
+        
+#         if annot_type != 'both':
+#             ax.annotate(
+#                 label, xy=(cx,cy), color='w', weight='bold', ha='center', va='center', fontsize=3)
+
+#     else:
+#         boxes = data['bbox']
+#         cx, cy = generate_center_point(boxes)
+
+#         for polygon in polygons:
+#             # print(polygon == 'counts') # 이상한거 하나 껴있음 ㅇㅅㅇ
+#             try:
+#                 polygon = np.asarray(polygon).reshape(-1, 2)
+#                 if polygon.shape[0] == 4:
+#                     continue
+#                 if color is None:
+#                     color = ((0.96, 0.26, 0.21, 0.2), (0.96, 0.26, 0.21, 1.0)) 
+#                 patch = Polygon(polygon, True, fc=color[0], ec=color[1], lw=0.5)
+#                 ax.add_patch(patch)
+#             except:
+#                 pass
+        
+#         if annot_type != 'both':
+#             ax.annotate(
+#                 label, xy=(cx,cy), color='w', weight='bold', ha='center', va='center', fontsize=3)
+
+
+# def draw_ground_truth(image_id=None, ax=None, annot_type='bbox'):
+#     for data in annotations['annotations']:
+#         if data['image_id'] == image_id:
+#             if annot_type == 'bbox':
+#                 draw_bbox(ax, data)
+#             elif annot_type == 'mask':
+#                 draw_mask(ax, data, pred=False)
+#             else:
+#                 draw_bbox(ax, data)
+#                 draw_mask(ax, data, pred=False)
+
+
+# def draw_prediction(image_id=None, ax=None, annot_type='bbox'):
+#     for bbox_data, mask_data in zip(bbox_detections, mask_detections):
+#         if bbox_data['image_id'] == image_id:
+#             if annot_type == 'bbox' and bbox_data['score'] > score_thresh:
+#                 draw_bbox(ax, bbox_data)
+#             elif annot_type == 'mask' and mask_data['score'] > score_thresh:
+#                 draw_mask(ax, mask_data, pred=True)
+#             elif annot_type == 'both' and mask_data['score'] > score_thresh:
+#                 draw_bbox(ax, bbox_data)
+#                 draw_mask(ax, mask_data, pred=True)
+
+# import random
+
+# def main():
+#     # Get image ID, and file names from ground truth data
+#     images = {annot['id']: annot['file_name'] for annot in annotations['images']}
+    
+#     shuffle = True
+#     if shuffle:
+#         shuffled_ids = list(images.keys())
+#         random.shuffle(shuffled_ids)
+    
+#     images = {key: images[key] for key in shuffled_ids}
+
+#     fig, axes = plt.subplots(num_imgs, 2, figsize=(6, 10), dpi=300)
+    
+#     i = 1
+#     for image_id, file_name in images.items():
+#         if path == 'custom':
+#             image = Image.open(f'./data/{path}/' + file_name)
+#         else:
+#             image = Image.open(coco_img_root + os.path.basename(file_name))
+
+#         axes.flatten()[i-1].imshow(image)
+#         axes.flatten()[i].imshow(image)
+        
+#         draw_prediction(image_id=image_id, ax=axes.flatten()[i-1], annot_type=annot_type)
+#         draw_ground_truth(image_id=image_id, ax=axes.flatten()[i], annot_type=annot_type)
+#         axes.flatten()[i-1].axis('off')
+#         axes.flatten()[i].axis('off')
+
+#         if i == (num_imgs * 2 - 1): # 39
+#             break
+
+#         i += 2
+
+#     fig.tight_layout()
+#     fig.savefig('./results/' + annot_type + '_' + path + '_' + str(score_thresh).replace('.','_') + '.png')
+#     if args.pop_up_img:
+#         plt.show()
+
+# if __name__ == '__main__':
+#     main()
+
+
