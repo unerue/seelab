65b7e380c388d32018a5ddd8d44abcf399d4eb84
unerue <unerue@me.com>
2020-12-15 14:25:30 +0900

    arange codes

--- seelab/misc/augmentations.py
+++ seelab/misc/augmentations.py
@@ -4,6 +4,34 @@ import numpy as np
 from torch import Tensor
 
 
+class Compose:
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, image, boxes=None, masks=None, labels=None):
+        for transform in self.transforms:
+            image, masks, boxes, labels = transform(
+                image, masks, boxes, labels)
+
+        return image, boxes, masks, labels
+
+class Resize:
+    def __init__(self, size, interpolation=Image.BILINEAR):
+        self.size = size
+        self.interpolation = interpolation
+
+    def __call__(self, image, boxes=None, masks=None, labels=None):
+        w, h = image.size
+        image = image.resize(self.size)
+        masks = TF.resize(masks, self.size, self.interpolation)
+
+        boxes[:, [0, 2]] *= self.size[0] / w
+        boxes[:, [1, 3]] *= self.size[1] / h
+
+        return image, boxes, masks, labels
+        
+
+
 class AddDarkness:
     """Add darkness
 
